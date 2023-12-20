46cf064ef29710027aed18f3cb0eb55514e2ad24
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-12-21 00:48:47 +0100

    i just add text

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,54 +1,59 @@
-// mainwindow.cpp
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <QLabel>
 #include <QPushButton>
 #include <QVBoxLayout>
 
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     // title of the game
     setWindowTitle("Sota");
+
     // icon
     setWindowIcon(QIcon(":/gfx/icon.ico"));
+
     // Set the background image using a stylesheet
     QString styleSheet = "background-color: #004d00;";
     this->setStyleSheet(styleSheet);
+
     // Create a QLabel for the centered image
     QLabel *centeredImage = new QLabel(this);
     centeredImage->setPixmap(QPixmap(":/gfx/home.png").scaled(600, 600, Qt::KeepAspectRatio));  // Replace with your image path
     centeredImage->setAlignment(Qt::AlignCenter);
 
-    // Create a QVBoxLayout to center the image with top spacing
+    // Create "Play Now" button
+    QPushButton *playNowButton = new QPushButton("Play Now", this);
+    playNowButton->setStyleSheet("QPushButton {"
+                                 "background-color: yellow;"
+                                 "border: 1px solid red;"
+                                 "border-radius: 10px;"
+                                 "padding: 15px;"
+                                 "}"
+                                 "QPushButton:hover {"
+                                 "background-color: lightyellow;"
+                                 "}");
+
+    // Create QLabel for white text
+    QLabel *whiteTextLabel = new QLabel("Sota", this);
+    whiteTextLabel->setStyleSheet("color: white;");  // Set the text color to white
+
+    // Create a QVBoxLayout to center the image and add the button
     QVBoxLayout *layout = new QVBoxLayout(this);
     layout->addSpacing(20);  // Adjust the spacing as needed
-    layout->addWidget(centeredImage);
+    layout->addWidget(centeredImage, 0, Qt::AlignCenter);  // Align the image to the center
+    layout->addWidget(whiteTextLabel, 0, Qt::AlignCenter);  // Align the white text to the center
+    layout->addWidget(playNowButton, 0, Qt::AlignCenter);  // Align the button to the center
+
     layout->setAlignment(Qt::AlignTop | Qt::AlignHCenter);
 
     // Set the central widget of the main window to a QWidget with the QVBoxLayout
     QWidget *centralWidget = new QWidget(this);
     centralWidget->setLayout(layout);
     setCentralWidget(centralWidget);
-
-    // Create "Play Now" button
-    QPushButton *playNowButton = new QPushButton("Play Now", this);
-    playNowButton->setStyleSheet("QPushButton {"
-                                 "background-color: yellow;"
-                                 "border: 2px solid red;"
-                                 "border-radius: 10px;"
-                                 "padding: 5px;"
-                                 "}"
-                                 "QPushButton:hover {"
-                                 "background-color: lightyellow;"
-                                 "}");
-    // Add the button to the layout
-    layout->addWidget(playNowButton);
-
-
 }
 
 MainWindow::~MainWindow()
