d30ac50bef64ab0e27d72c64bc4cf29592da27e9
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-12-20 20:47:50 +0100

    i add the cards

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 // mainwindow.cpp
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -8,6 +9,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    // title of the game
     // title of the game
     setWindowTitle("Sota");
     // setWindowIcon(QIcon(":/path/to/your/icon.png"));
@@ -17,3 +19,13 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QMessageBox::warning(
+        this,
+        tr("sota"),
+        tr("you still have double carts") );
+}
+
